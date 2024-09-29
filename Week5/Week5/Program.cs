152f400bf71677030fa6a0c87bf9d43cee2d223c
lydiajin1 <87837350+lydiajin1@users.noreply.github.com>
2024-09-29 19:52:27 -0400

    Add files via upload

---
+++ Week5/Week5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Week5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //adding numbers 
+            Console.WriteLine("Hello, World!");
+            int myFavNum = 26;
+            int theBestNum = 1;
+
+            int sum;
+
+            sum = myFavNum + theBestNum;
+
+            Console.WriteLine(sum);
+
+            //Professor Name 
+            string professorName = "Nick";
+            Console.WriteLine("Professor's Name: " + professorName);
+
+
+            //favorite color 
+            string favoriteColor = "Blue";
+            Console.WriteLine("My favorite color is " + favoriteColor);
+
+
+
+
+        }
+    }
+}
